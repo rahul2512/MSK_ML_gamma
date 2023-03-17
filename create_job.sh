2c5c264f3724678a406d1d1dc1e03b274376b70b
rahul2512 <rs25.iitr@gmail.com>
2023-03-17 16:00:22 +0100

    LM_update

--- create_job.sh
+++ create_job.sh
@@ -1,20 +1,20 @@
 ##  1270080 total number of hyp
-rm jobs/NN_job*slurm
+rm jobs/LM_job*slurm
 count=0
-for (( i=0; i<9721; i+=1 )) 
+for (( i=0; i<55; i+=1 )) 
 do
 index=$[count/36+1]
 count=$[count+1]
-echo "python " '${path}'"/main.py" ${i} "&" >> NN_job_${index}.slurm
+echo "python " '${path}'"/main.py" ${i} "&" >> LM_job_${index}.slurm
 done 
 
-for index in {1..271}
+for index in {1..2}
 do 
 touch tmp 
 cat heading.txt >> tmp
-cat NN_job_${index}.slurm >> tmp 
+cat LM_job_${index}.slurm >> tmp 
 echo "wait" >> tmp
-rm NN_job_${index}.slurm
-mv tmp jobs/NN_job_${index}.slurm
-sed -i "s/MMM/NN_${index}/g" jobs/NN_job_${index}.slurm
+rm LM_job_${index}.slurm
+mv tmp jobs/LM_job_${index}.slurm
+sed -i "s/MMM/LM_${index}/g" jobs/LM_job_${index}.slurm
 done
