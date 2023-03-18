c4a82ab18df1e1efa173eb4f0366cbfc8045bae9
rahul2512 <rs25.iitr@gmail.com>
2023-03-18 01:27:39 +0100

    jobs_RNN

--- create_job.sh
+++ create_job.sh
@@ -1,20 +1,20 @@
 ##  1270080 total number of hyp
-rm jobs/LM_job*slurm
+rm jobs/RNN_job*slurm
 count=0
-for (( i=0; i<55; i+=1 )) 
+for (( i=0; i<5185; i+=1 )) 
 do
 index=$[count/36+1]
 count=$[count+1]
-echo "python " '${path}'"/main.py" ${i} "&" >> LM_job_${index}.slurm
+echo "python " '${path}'"/main.py" ${i} "&" >> RNN_job_${index}.slurm
 done 
 
-for index in {1..2}
+for index in {1..145}
 do 
 touch tmp 
 cat heading.txt >> tmp
-cat LM_job_${index}.slurm >> tmp 
+cat RNN_job_${index}.slurm >> tmp 
 echo "wait" >> tmp
-rm LM_job_${index}.slurm
-mv tmp jobs/LM_job_${index}.slurm
-sed -i "s/MMM/LM_${index}/g" jobs/LM_job_${index}.slurm
+rm RNN_job_${index}.slurm
+mv tmp jobs/RNN_job_${index}.slurm
+sed -i "s/MMM/RNN_${index}/g" jobs/RNN_job_${index}.slurm
 done
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,4 +1,4 @@
-for i in {1..271}
+for i in {1..145}
 do 
 echo "Submitting job ${i}"
 sbatch jobs/NN_job_${i}.slurm 
