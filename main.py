2e7e8bbf2281fb7c05225fac55fa1445da5b06d7
rahul2512 <rs25.iitr@gmail.com>
2023-03-31 13:10:02 +0200

    CNN

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd, keras
 import os.path
 from pathlib import Path
 
-from pytorch import run_final_model, run_cross_valid, check_interpolation, combined_plot, save_outputs , plot_MSK_data
+from pytorch import run_final_model, run_cross_valid, check_interpolation, combined_plot, save_outputs 
 from pytorch import RNN_models,  feature_slist, feature_list, print_optimal_results, stat, specific_CV, specific, print_SI_table1, print_SI_table2, explore
 from pytorch import print_SI_table3
 from read_in_out import initiate_data, initiate_RNN_data, analysis_options, ML_analysis
@@ -14,23 +14,27 @@ from barchart_err import barchart_error, barchart_params
 window = 20
 data_kind  =  ['LM', 'NN', 'RNN', 'CNN', 'CNNLSTM', 'convLSTM']
 data_kind  =  ['NN','LM']
+data_kind  =  ['CNN']
 fm = ML_analysis('final_model_list', data_kind, window)
 
-fm.LM.exposed.arg      = [12, 12, 12]
-fm.LM.naive.arg        = [12, 12, 12]
-fm.LM.exposed.arch     = ['LM']*3
-fm.LM.naive.arch       = ['LM']*3
+should = 0 
 
-fm.NN.exposed.arg        = [1080, 1118, 2809]
-fm.NN.naive.arg          = [1080, 2164, 6778]
-fm.NN.exposed.arch       = ['NN']*3
-fm.NN.naive.arch         = ['NN']*3
+if should:
+    fm.LM.exposed.arg      = [12, 12, 12]
+    fm.LM.naive.arg        = [12, 12, 12]
+    fm.LM.exposed.arch     = ['LM']*3
+    fm.LM.naive.arch       = ['LM']*3
 
-# fm.RNN.exposed.arg       = [1252, 1836, 1537, 1489, 1416]
-# fm.RNN.exposed.arch      = ['BLSTM','LSTM','BLSTM','LSTM','LSTM']
+    fm.NN.exposed.arg        = [1080, 1118, 2809]
+    fm.NN.naive.arg          = [1080, 2164, 6778]
+    fm.NN.exposed.arch       = ['NN']*3
+    fm.NN.naive.arch         = ['NN']*3
 
-# fm.RNN.naive.arg         = [     52,     36,   694,  1037,  1934]  
-# fm.RNN.naive.arch        = ['BLSTM', 'LSTM', 'GRU', 'GRU', 'LSTM']
+    # fm.RNN.exposed.arg       = [1252, 1836, 1537, 1489, 1416]
+    # fm.RNN.exposed.arch      = ['BLSTM','LSTM','BLSTM','LSTM','LSTM']
+
+    # fm.RNN.naive.arg         = [     52,     36,   694,  1037,  1934]  
+    # fm.RNN.naive.arch        = ['BLSTM', 'LSTM', 'GRU', 'GRU', 'LSTM']
 
 
 def train_final_models(fm):
@@ -78,7 +82,7 @@ def avg_stat(fm):
         print('%',np.around(np.mean(a),2),np.around(np.std(a),2), j.kind, j.subject, 'NRMSE')
         print('%',np.around(np.mean(b),2),np.around(np.std(b),2), j.kind, j.subject, 'pc')
 
-# hyper_index = int(sys.argv[1])
-# explore(fm.CNN, hyper_index)
+hyper_index = int(sys.argv[1])
+explore(fm.CNN, hyper_index)
 # fm = compute_stat(fm)
 # plot_final_results(fm)
