535d8488b1d606abc1440753375b81f33276c601
Rahul Sharma <38540112+rahul2512@users.noreply.github.com>
2023-05-15 17:52:06 +0200

    rf

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,3 @@
-git add *.sh jobs plots_out lc_data Input Output README.md  *txt *py 
+git add *.sh jobs plots_out lc_data Input Output hyperparameters README.md  *txt *py 
 git commit -m $1 
 git push git@github.com:rahul2512/MSK_ML_gamma.git
