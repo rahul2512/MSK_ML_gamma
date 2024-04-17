0d35820ce82f9cd1f4c48fa30c6485fc1127ed4d
rahul2512 <rs25.iitr@gmail.com>
2024-04-17 22:23:20 +0100

    hj

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,3 @@
 git add *.sh jobs plots_out lc_data Input Output hyperparameters README.md  *txt *py 
 git commit -m $1 
-git push git@github.com:rahul2512/MSK_ML_gamma.git
+git push main git@github.com:rahul2512/MSK_ML_gamma.git --force
